073a42c717cb26168b69987fa5ae8961d0cf16cb
mateusgoto01 <mateusgoto01@users.noreply.github.com>
2020-07-16 11:33:39 -0300

    [UPDATED] Adicionado mais comentarios e print dos sensores individuais

--- Multisonic/Multisonic.cpp
+++ Multisonic/Multisonic.cpp
@@ -51,7 +51,7 @@ void Multisonic::AvaregeDistance() {
     delayMicroseconds(10);
     digitalWrite(pin1, LOW);
     pulse1 = pulseIn(pin2, HIGH); // capita o tempo de nivel alto do echo
-    dist1 = pulse1/58.82; // Formula de velocidade média do som para achar a distância
+    dist1 = pulse1/58.82; // Formula de velocidade média do som para achar a distância em cm
     // Repete o processo acima para o segundo sensor
     digitalWrite(pin3, HIGH);
     delayMicroseconds(10);
@@ -71,8 +71,11 @@ void Multisonic::AvaregeDistance() {
     pulse4 = pulseIn(pin8, HIGH);
     dist4 = pulse4/58.82;
 
-    Avarege = (dist1 + dist2 + dist3 + dist4)/4; // realiza a média aritimética das distâncias
-
-    Serial.println(Avarege); // Mostra no serial monitor a média aritimética das distâncias
+    Avarege = (dist1 + dist2 + dist3 + dist4)/4; // realiza a média aritimética das distâncias em cm
+    Serial.print('Primeiro sensor: '); Serial.print(dist1); // Printa os valores individuais de cada sensor, lembrando que é em cm
+    Serial.print('Segundo sensor: '); Serial.print(dist2);
+    Serial.print('Terceiro sensor: '); Serial.print(dist3);
+    Serial.print('Quarto sensor: '); Serial.print(dist4);
+    Serial.println(Avarege); // Mostra no serial monitor a média aritimética das distâncias em cm
     
 }
