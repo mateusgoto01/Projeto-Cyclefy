6136e369417dbdc49a2a120607d0d5349e63dbd3
mateusgoto01 <mateusgoto01@users.noreply.github.com>
2020-08-22 16:26:34 -0300

    [New] Versão final

---
+++ LoRa2/LoRa2.ino
@@ -0,0 +1,49 @@
+#include <RHReliableDatagram.h>
+#include <RH_RF95.h>
+#include <SPI.h>
+
+#define CLIENT_ADDRESS 2
+#define SERVER_ADDRESS 1
+
+// Singleton instance of the radio driver
+RH_RF95 driver;
+//RH_RF95 rf95(5, 2); // Rocket Scream Mini Ultra Pro with the RFM95W
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram manager(driver, CLIENT_ADDRESS);
+
+
+
+
+void setup() 
+{
+  // Rocket Scream Mini Ultra Pro with the RFM95W only:
+  // Ensure serial flash is not interfering with radio communication on SPI bus
+//  pinMode(4, OUTPUT);
+//  digitalWrite(4, HIGH);
+
+  Serial.begin(9600);
+  while (!Serial) ; // Wait for serial port to be available
+  if (!manager.init())
+    Serial.println("init failed");
+    driver.setFrequency(868);
+
+}
+
+
+void loop()
+{
+  float peso = 300.5125;
+  float altura = 3.7125;
+
+  String data_string = String(peso) + "$" + String(altura);
+  char data[12];
+  data_string.toCharArray(data, 12);
+  Serial.println(data_string);
+  manager.sendtoWait((uint8_t *)data, sizeof(data), SERVER_ADDRESS);
+  manager.waitPacketSent();
+  Serial.println("Mensagem enviada");
+  delay(1000); // 1 segundos até a proxima atualização
+
+
+}
---
+++ Server/Server.py
@@ -0,0 +1,46 @@
+from raspi_lora import LoRa, ModemConfig
+import pyrebase
+
+
+config = {
+  "apiKey": "AIzaSyCWH1RjnADOMq_vjOJmelWc0uYOPZ8AX_0",
+  "authDomain": "cyclefy-5f768.firebaseapp.com",
+  "databaseURL": "https://cyclefy-5f768.firebaseio.com",
+  "storageBucket": "cyclefy-5f768.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+
+def on_recv(payload):
+    if payload.header_from == 2:
+        print("From: ", payload.header_from)
+        print("Received: ", payload.message)
+        dados = bytes(payload.message).decode("utf-8")
+        print(dados)
+        data_list = [str(data) for data in dados.split('$')]
+        print("Peso: ", data_list[0])
+        print("Altura: ", data_list[1][:-1])
+        
+        db.child("lixeira 1").child("Peso:").set(data_list[0])
+        db.child("lixeira 1").child("Altura:").set(data_list[1][:-1])
+    if payload.header_from == 3:
+        print("From: ", payload.header_from)
+        print("Received: ", payload.message)
+        dados = bytes(payload.message).decode("utf-8")
+        print(dados)
+        data_list = [str(data) for data in dados.split('$')]
+        print("Peso: ", data_list[0][:-1])
+        print("Altura: ", data_list[1])
+        db.child("lixeira 2").child("Peso:").set(data_list[0])
+        db.child("lixeira 2").child("Altura:").set(data_list[1][:-1])
+
+
+
+lora = LoRa(0, 17, 1, freq=868, modem_config=ModemConfig.Bw125Cr45Sf128, tx_power=14, acks=True)
+
+print("iniciando...")
+lora.on_recv = on_recv
+lora.set_mode_rx()
+print("Finalizado")
